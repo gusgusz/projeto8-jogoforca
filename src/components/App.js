1fed0c3020d30e88be2e96a470aa30bacd32d736
Gustavo <gustavoguimaraescamps@gmail.com>
2022-09-20 17:45:51 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+export default function App(){
+
+    return(
+        <div>oiiiiikxxxxxxxxxxxxxxxx
+            dledkçedk
+            dkkdkdkd
+            ddkddkd
+            ddkddkdddd
+            ddkddkddddd
+            ddkddkddddd
+        </div>
+    )
+}
